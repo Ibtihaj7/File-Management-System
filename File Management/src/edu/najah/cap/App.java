7b8fad9e32c2b9ca10dc99e3eadb56ac8f3ea7bb
Ibtihaj7 <ibtihaj.adham7@gmail.com>
2022-12-15 14:02:22 +0200

    connect database

--- File Management/src/edu/najah/cap/App.java
+++ File Management/src/edu/najah/cap/App.java
@@ -1,7 +1,25 @@
 package edu.najah.cap;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class App {
     public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/File_Management", "root", "password");
 
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM files");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name"));
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
