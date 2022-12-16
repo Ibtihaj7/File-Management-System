3367168e587b1467f289693fdf370623c99f56e9
abrarhannoon <92648528+abrarhannoon@users.noreply.github.com>
2022-12-17 00:00:34 +0200

    edit

--- File Management/src/edu/najah/cap/FileRepository/impl/NormalDelete.java
+++ File Management/src/edu/najah/cap/FileRepository/impl/NormalDelete.java
@@ -2,12 +2,16 @@ package edu.najah.cap.FileRepository.impl;
 
 import edu.najah.cap.Database.impl.MySQLDatabase;
 import edu.najah.cap.FileRepository.intf.Delete;
+import edu.najah.cap.Security.Authorization;
 
 public class NormalDelete implements Delete {
 
     @Override
     public void delete(String filename, String category) {
         try {
+            if(!Authorization.hasAdminPermission()){
+                return;
+            }
             String query = "DELETE FROM files WHERE name = "+filename+" AND category = "+category+";";
             MySQLDatabase.getInstance().execute(query);
         }catch (Exception e){
